8c5c1d94e7a0e9422b3bbdf6adeb880a9afb1e24
Gunal1509 <sivagunal15@gmail.com>
2026-06-30 23:41:52 +0530

    week 3

---
+++ week 3/Spring Rest using Spring Boot/hands on 4/spring-learns/src/main/java/com/cognizant/spring_learns/SpringLearnsApplication.java
@@ -0,0 +1,29 @@
+package com.cognizant.spring_learns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+@SpringBootApplication
+public class SpringLearnsApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringLearnsApplication.class, args);
+
+        displayCountry();
+    }
+
+    public static void displayCountry() {
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("country.xml");
+
+        Country country = context.getBean("country", Country.class);
+
+        System.out.println(country);
+    }
+
+}
